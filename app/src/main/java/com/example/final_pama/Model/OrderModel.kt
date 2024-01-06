5dda30928d2742ba4736f12de739ce56e2746957
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2024-01-06 16:20:50 +0700

    nambahin order model

--- app/src/main/java/com/example/final_pama/Model/OrderModel.kt
+++ app/src/main/java/com/example/final_pama/Model/OrderModel.kt
@@ -1,3 +1,14 @@
 package com.example.final_pama.Model
 
-data class OrderModel()
+data class OrderModel(
+    val order: Order,
+    val message: String
+)
+data class Order (
+    val  id : Int,
+    val user_id: Int,
+    val data: String,
+    val created_at: String,
+    val update_at: String
+
+)
