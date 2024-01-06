0eb27ada7df7eff2a91c92b66ad1256d5f749cc8
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2024-01-06 16:32:52 +0700

    Nambahin EndPoint

--- app/src/main/java/com/example/final_pama/api/ApiEndPoint.kt
+++ app/src/main/java/com/example/final_pama/api/ApiEndPoint.kt
@@ -3,11 +3,16 @@ package com.example.final_pama.api
 import android.provider.ContactsContract.CommonDataKinds.Email
 import android.telecom.Call
 import com.example.final_pama.Model.MovieModel
+import com.example.final_pama.Model.OrderModel
+import com.example.final_pama.Model.ResponseModel
+import com.example.final_pama.Model.SeatModel
 import com.example.final_pama.Model.UserModel
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiEndPoint {
 
@@ -21,4 +26,20 @@ interface ApiEndPoint {
 
     @GET("movie")
     fun getmovie(): retrofit2.Call<MovieModel>
+
+    @GET("movie/{id}")
+    fun showMovie(@Path("id")id: Int ): retrofit2.Call<MovieModel>
+
+    @GET("seat")
+    fun getSeat(@Query("movie_schedule_id") id: Int, @Query("screening_date")
+    date: String): retrofit2.Call<SeatModel>
+
+    @POST("order")
+    fun createOrder(): retrofit2.Call<OrderModel>
+
+    @FormUrlEncoded
+    @POST("order/detail")
+    fun createdOrderDetail(@Field("order_id") order: Int, @Field
+        ("movie_schedule_id") schedulee: Int, @Field("seat_id") seat: Int, @Field
+                               ("movie_screening") date: String): retrofit2.Call<ResponseModel>
 }
