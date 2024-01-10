cd62da42264b34466bfc087ae4c9135e18145ec9
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2024-01-10 14:29:24 +0700

    nambahin custom top bar, tiket grid dan tiket card

--- app/src/main/java/com/example/final_pama/MyTicket.kt
+++ app/src/main/java/com/example/final_pama/MyTicket.kt
@@ -3,16 +3,41 @@ package com.example.final_pama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.final_pama.Model.Ticket
+import com.example.final_pama.api.ApiRetrofit
 import com.example.final_pama.ui.theme.Final_PamATheme
+import retrofit2.awaitResponse
 
 class MyTicket : ComponentActivity() {
+
+    private val api by lazy { ApiRetrofit().apiEndPoint }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,25 +47,82 @@ class MyTicket : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
+
+    }
+    @Composable
+    private fun TicketGrid() {
+        var ticket by remember {
+            mutableStateOf<List<Ticket>>(emptyList())
+        }
+        LaunchedEffect(key1 = Unit) {
+            getTicket()?.let {
+                ticket = it
+            }
+        }
+
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(1),
+            modifier = Modifier.padding(16.dp)
+        ) {
+            items(ticket) { ticket ->
+                TicketCard(ticket = ticket)
+            }
+        }
+    }
+        private suspend fun getTicket(): List<Ticket>?{
+            return try {
+                val response = api.getTicket().awaitResponse()
+                if (response.isSuccessful){
+                    response.body()?.ticket
+                }else{
+                    null
+                }
+            } catch (e: Exception){
+                null
+            }
+        }
+    }
+
+@Composable
+private fun TicketCard(ticket: Ticket){
+
+}
+@Composable
+private fun CustomTopBar(){
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp
+    val isWideScreen = screenWidthDp.dp > 600.dp
+
+    val topBarHeight = if (isWideScreen)118.dp else 50.dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(topBarHeight)
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(
+                    bottomStart = 30.dp,
+                    bottomEnd = 30.dp
+                )
+            ),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Text(
+            text = "Movie List",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            ),
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .fillMaxSize(),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview3() {
-    Final_PamATheme {
-        Greeting("Android")
-    }
-}
