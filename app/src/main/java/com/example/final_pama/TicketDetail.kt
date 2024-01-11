20fdcf21a759048350e3f103d2fa6354e2cae1bc
Arfan-maulana <114915872+Arfan-maulana@users.noreply.github.com>
2024-01-11 12:32:23 +0700

    nambahin custom top bar halaman ticket detail

--- app/src/main/java/com/example/final_pama/TicketDetail.kt
+++ app/src/main/java/com/example/final_pama/TicketDetail.kt
@@ -3,13 +3,25 @@ package com.example.final_pama
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.final_pama.ui.theme.Final_PamATheme
 
 class TicketDetail : ComponentActivity() {
@@ -22,7 +34,7 @@ class TicketDetail : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                  CustomTopBar()
                 }
             }
         }
@@ -37,10 +49,38 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview3() {
-    Final_PamATheme {
-        Greeting("Android")
+private fun CustomTopBar(){
+    val configuration = LocalConfiguration.current
+    val screenWidthDp = configuration.screenWidthDp
+    val isWideScreen = screenWidthDp.dp > 600.dp
+
+    val topBarHeight = if (isWideScreen)118.dp else 50.dp
+
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(topBarHeight)
+            .background(
+                color = MaterialTheme.colorScheme.primary,
+                shape = RoundedCornerShape(
+                    bottomStart = 30.dp,
+                    bottomEnd = 30.dp
+                )
+            ),
+        contentAlignment = Alignment.TopCenter
+    ){
+        Text(
+            text = "Ticket Detail",
+            style = MaterialTheme.typography.headlineMedium.copy(
+                fontWeight = FontWeight.Bold,
+                color = Color.White
+            ),
+            modifier = Modifier
+                .padding(bottom = 8.dp)
+                .fillMaxSize(),
+            textAlign = TextAlign.Center
+        )
     }
 }
+
